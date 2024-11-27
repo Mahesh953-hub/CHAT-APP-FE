2a0fef178d33e01b10b9cc2b14be78d41b6704d4
MAHESH-OP <alex.c992954@gmail.com>
2024-11-27 20:59:52 +0530

    Chat-FE

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Find the root element to attach React app
+const root = document.getElementById("root")!;
+
+// Wait until the window has fully loaded to hide the loading spinner and show the content
+window.onload = () => {
+  document.body.classList.add("loaded");
+};
+
+// Render the React app
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
