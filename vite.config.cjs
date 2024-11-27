32fa92b13f3ebbc9da65957372cf9cc2fdb14901
MAHESH-OP <alex.c992954@gmail.com>
2024-11-27 20:23:41 +0530

    Chat-FE

---
+++ vite.config.cjs
@@ -0,0 +1,22 @@
+"use strict";
+const { defineConfig } = require("vite");
+const react = require("@vitejs/plugin-react");
+const path = require("path");
+
+module.exports = defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@hooks': path.resolve(__dirname, './src/hooks'),
+            '@store': path.resolve(__dirname, './src/store'),
+            '@utils': path.resolve(__dirname, './src/utils'),
+        },
+    },
+    build: {
+        outDir: 'dist',
+        sourcemap: true,
+    },
+    assetsInclude: ['**/*.js', '**/*.css'], // Ensure .js and .css files are included
+});
