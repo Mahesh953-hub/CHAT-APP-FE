379dd32e466d42d81e885b62a3dcfcff9603b14f
MAHESH-OP <alex.c992954@gmail.com>
2024-11-28 06:38:41 +0530

    Chat-FE

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
-export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3000';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://chat-app-be00.onrender.com';
+export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'https://chat-app-ws-jrj3.onrender.com';
 
 export const THEME_STORAGE_KEY = 'theme-storage';
 export const USER_STORAGE_KEY = 'user-storage';
