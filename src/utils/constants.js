3241bae4dfa6b2db53441b65f8b6d4c50973202a
MAHESH-OP <alex.c992954@gmail.com>
2024-11-28 06:40:16 +0530

    Chat-FE

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.USER_STORAGE_KEY = exports.THEME_STORAGE_KEY = exports.SOCKET_URL = exports.API_URL = void 0;
-exports.API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
-exports.SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3000';
+exports.API_URL = import.meta.env.VITE_API_URL || 'https://chat-app-ws-jrj3.onrender.com:3000';
+exports.SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'https://chat-app-ws-jrj3.onrender.com:3000';
 exports.THEME_STORAGE_KEY = 'theme-storage';
 exports.USER_STORAGE_KEY = 'user-storage';
