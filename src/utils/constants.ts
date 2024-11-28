edf466589a8e77554e1a4580229bc76ff614938b
MAHESH-OP <alex.c992954@gmail.com>
2024-11-28 07:03:43 +0530

    Chat-FE

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,4 +2,4 @@ export const API_URL = import.meta.env.VITE_API_URL || 'https://chat-app-be00.on
 export const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'https://chat-app-ws-jrj3.onrender.com';
 
 export const THEME_STORAGE_KEY = 'theme-storage';
-export const USER_STORAGE_KEY = 'user-storage';
+export const USER_STORAGE
